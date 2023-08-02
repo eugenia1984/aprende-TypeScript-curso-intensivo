d37c6507f98d5e91a985ac3c5b11fc418cb3f701
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-08-02 13:39:08 -0300

    -feat: sigo con los tipo de datos complejos

--- open_bootcamp/ts-demo/src/index.ts
+++ open_bootcamp/ts-demo/src/index.ts
@@ -23,20 +23,20 @@ console.log(`El e-mail de ${ firstName } es: ${ eMail }`)
 
 // Ejemplo de constante
 // no se puede reasignar un valor, no se modifica
-const PI: number = 3.1416 
+const PI: number = 3.1416
 
 
 // Ejemplo de boolean
-let error:boolean = false
+let error: boolean = false
 // aca con .toString no es boolean sino es string
-console.log(`¿Hay error?: ${error}`) 
+console.log(`¿Hay error?: ${ error }`)
 // Mejor declarar el tipo si solo declaro la variable,
 // sino despues cuando asigno un valor podria ser cualquier cosa menos boolean
-let esPrimo: boolean 
+let esPrimo: boolean
 
 
 // instancia 3 variables sin valor inicial
-let a:string, b:boolean, c:number;
+let a: string, b: boolean, c: number;
 a = 'TYpeScript'
 b = true
 c = 2.34
@@ -46,7 +46,57 @@ c = 2.34
 // Se pierde el tipado, se puede asignar distintos tipos
 // de datos a una misma variable
 
-/************  Tipo de datos complejos, no primitivos  **********/
+/************  Tipo de datos complejos, no primitivos  ***********/
 // se pasan por referencia en vez de por valor
-// Ejemplo de array
+
+/********************  Ejemplo de array **************************/
 let taskList: string[] = ['Task 1', 'Task 2', 'Task 3']
+
+// Ejemplo de combinar tipos en arrays
+// Puedo tener valores que sean: string, number o boolean
+let values: (string | number | boolean)[] = [false, 'Hola', true, 56]
+
+/************************  Ejemplo de enum  ***********************/
+enum Estado {
+  'Completado' = 'C',
+  'Incompleto' = 'I',
+  'Pendiente' = 'P'
+}
+
+let estadoTareas: Estado = Estado.Completado
+// el valor que guardo es 'C'
+
+enum PuestoCarrera {
+  'Primero' = 1,
+  'Segundo',
+  'Tercero'
+}
+let puestoMaraton: PuestoCarrera = PuestoCarrera.Segundo
+// el valor que guardo es 2
+
+/*****************  Ejemplo de interface ***************/
+interface Tarea {
+  nombre: string,
+  estado: Estado,
+  urgencia: number
+}
+
+// Podemos crear variables que sigan la interface Tarea
+let tarea1: Tarea = {
+  nombre: 'Tarea 1',
+  estado: Estado.Pendiente,
+  urgencia: 10
+}
+console.log(`Tarea: ${ tarea1.nombre }`)
+// Type de TypeScript
+// Los tipos son para definir un tipo mas complejo
+type Producto = {
+  precio: number,
+  nombre: string
+}
+
+let coche: Producto = {
+  nombre: 'Audi',
+  precio: 45000
+}
+console.log(`Producto: ${ coche.nombre }`)
